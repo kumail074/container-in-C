d4623dee293fe65a402e1d7b04cd862685dfc42d
kumail074 <kumail074@proton.me>
2024-11-25 02:50:46 +0500

    more code

--- cont.c
+++ cont.c
@@ -35,6 +35,11 @@ typedef struct {
     char *mount_dir;
 } child_config;
 
+// pivot-root
+int pivot_root(const char *mew_root, const char *put_old) {
+    return syscall(SYS_pivot_root, new_root, put_old);
+}
+
 // capabilities
 
 int capabilities() {
@@ -51,10 +56,10 @@ int capabilities() {
         CAP_MAC_OVERRIDE,
         CAP_MKNOD,
         CAP_SETFCAP,
-        CAP_SYSLOG,
-        CAP_SYS_ADMIN,
-        CAP_SYS_BOOT,
-        CAP_SYS_MODULE,
+        //CAP_SYSLOG,
+        //CAP_SYS_ADMIN,
+        //CAP_SYS_BOOT,
+        //CAP_SYS_MODULE,
         CAP_SYS_NICE,
         CAP_SYS_RAWIO,
         CAP_SYS_RESOURCE,
@@ -84,9 +89,216 @@ int capabilities() {
 
 // mounts
 
+int mounts(child_config *config) {
+    fprintf(stderr, "=> remounting everything wih MS_PRIVATE...");
+    if(mount(NULL, "/", NULL, MS_REC | MS_PRIVATE, NULL)) {
+        fprintf(stderr, "failed! %m\n");
+        return -1;
+    }
+    fprintf(stderr, "remounted.\n");
+
+    fprintf(stderr, "=> making a temp directory!\n");
+    char mount_dir[] = "/tmp/tmp.XXXXXX";
+    if(!mkdtemp(mount_dir)) {
+        fprintf(stderr, "failed making a directory\n");
+        return -1;
+    }
+
+    if(mount(config->mount_dir, mount_dir, NULL, MS_BIND | MS_PRIVATE, NULL)) {
+        fprintf(stderr, "bind mount failed\n");
+        return -1;
+    }
+
+    char inner_mount_dir[] = "/tmp/tmp.XXXXXX/oldroot.XXXXXX";
+    memcpy(inner_mount_dir, mount_dir, sizeof(mount_dir) - 1);
+    if(!mkdtemp(inner_mount_dir)) {
+        fprintf(stderr, "failed making the inner directory\n");
+        return -1;
+    }
+
+    fprintf(stderr, "done.\n");
+
+    fprintf(stderr, "=> pivoting root...");
+    if(pivot_root(mount_dir, inner_mount_dir)) {
+        fprintf(stderr, "failed!\n");
+        return -1;
+    }
+    fprintf(stderr, "done\n");
+
+    char *old_root_dir = basename(inner_mount_dir);
+    char old_root[sizeof(inner_mount_dir) + 1] = { "/" };
+    strcpy(&old_root[1], old_root_dir);
+
+    fprintf(stderr, "=> unmounting &s...", old_root);
+    if(chdir("/")) {
+        forintf(stderr, "chdir failed: %m\n");
+        return -1;
+    }
+    if(umount2(old_root, MNT_DETACH)) {
+        fprintf(stderr, "umount failed: %m\n");
+        return -2;
+    }
+
+    if(rmdir(old_root)) {
+        fprintf(stderr, "rmdir failed: %m\n");
+        return -1;
+    }
+    fprintf(stderr, "done\n");
+    return 0;
+}
+
 // syscalls
 
+#define SCMP_FAIL SCMP_ACT_ERRNO(EPERM)
+
+int syscalls() {
+    scmp_filter_ctx ctx = NULL;
+    fprintf(stderr, "=> filtering syscalls...");
+    if(!(ctx = seccomp_init(SCMP_ACT_ALLOW))
+            || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(chmod), 1, SCMP_A1(ACMP_CMP_MASKED_EQ, S_ISUID, S_ISUID)) 
+            || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(chmod), 1, SCMP_A1(SCMP_CMP_MASKED_EQ, S_ISGID, S_ISGID))
+            || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(fchmod), 1, SCMP_A1(SCMP_CMP_MASKED_EQ, S_ISUID, S_ISUID))
+            || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(fchmod), 1, SCMP_A1(SCMP_CMP_MASKED_EQ, S_ISGID, IS_GID))
+            || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(fchmodat), 1, SCMP_A2(SCMP_CMP_MASKED_EQ, S_ISUID, S_ISUID))
+            || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(fchmodat), 1, SCMP_A2(SCMP_SMP_MASKED_EQ, S_ISGID, S_ISGID))
+            || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(unshare), 1, SCMP_A0(SCMP_CMP_MASKED_EQ, CLONE_NEWUSER, CLONE_NEWUSER))
+            || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(clone), 1, SCMP_A0(SCMP_CMP_MASKED_EQ, CLONE_NEWUSER, CLONE_NEWUSER))
+            || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(ioctl), 1, SCMP_A1(SCMP_CMP_MASKED_EQ, TIOCSTI, TIOCSTI))
+            || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(keyctl), 0)
+            || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(add_key), 0)
+            || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(request_key), 0)
+            || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(ptrace), 0)
+            || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(mbind), 0)
+            || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(migrate_pages), 0)
+            || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(move_pages), 0)
+            || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(set_mempolicy), 0)
+            || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(userfaultfd), 0)
+            || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(perf_event_open), 0)
+            || seccomp_attr_set(ctx, SCMP_FLTATR_CTL_NNP, 0)
+            || seccomp_load(ctx)) {
+                if(ctx) seccomp_release(ctx)l
+                    fprintf(stderr, "failed: %m\n");
+                return -1;
+            }
+    seccomp_release(ctx);
+    fprintf(stderr, "done\n");
+    return 0;
+}
+
 // resources
+#define MEMORY "1073741824"
+#define SHARES "256"
+#define PIDS "64"
+#define WEIGHT "10"
+#define FD_COUNT 64
+
+struct cgrp_control {
+    char control[256];
+    struct cgrp_setting {
+        char name[256];
+        char value[256];
+    } **settings;
+};
+struct cgrp_setting add_to_tasks = {
+    .name = "tasks",
+    .value = "0"
+};
+
+struct cgrp_control *cgrps[] = {
+    & (struct cgrp_control) {
+        .control = "memory",
+        .settings = (struct cgrp_setting *[]) {
+            &(struct cgrp_setting) {
+            .name = "memory.limit_in_bytes",
+            .value = MEMORY
+        },
+        & (struct cgrp_setting) {
+            .name = "memory.kmem.limit_in_bytes",
+            .value = MEMORY
+        },
+        &add_to_tasks,
+        NULL
+        }
+    },
+    &(struct cgrp_control) {
+        .control = "cpu",
+        .settings = (struct cgrp_setting *[]) {
+            &(struct cgrp_setting) {
+                .name = "cpu.shares",
+                .value = SHARES
+            },
+            &add_to_tasks,
+            NULL
+        }
+    },
+    &(struct cgrp_control) {
+        .control = "pids",
+        .settings = (struct cgrp_setting *[]) {
+            &(struct cgrp_setting) {
+                .name = "pids.max",
+                .value = PIDS
+            },
+            &add_to_tasks,
+            NULL
+        }
+    },
+    &(struct cgrp_control) {
+        .control = "blkio",
+        .settings = (struct cgrp_setting *[]) {
+            & (struct cgrp_setting) {
+                .name = "blkio,weight",
+                .value = PIDS
+            },
+            &add_to_tasks,
+            NULL
+        }
+    },
+    NULL
+};
+
+int resources(struct child_config *config) {
+    fprintf(stderr, "=> setting cgroups...");
+    for(struct cgrp_control **cgrp = cgrps; *cgrp; cgrp++) {
+        char dir[PATH_MAX] = {0};
+        fprintf(stderr, "%s...", (*cgrp)->control)l
+            if(snprintf(dir, sizeof(dir), "/sys/fs/cgroup/%s/%s", (*cgrp)->control, config->hostname) == -1) {
+                return -1;
+            }
+        if(mkdir(dir, S_IRUSR | S_IWUSR | S_IXUSR)) {
+            fprintf(stderr, "mkdir %s failed: %m\n", dir);
+            return -1;
+        }
+        for(struct cgrp_setting **setting = (*cgrp)->settings; *setting; setting++) {
+            char path[PATH_MAX] = {0};
+            int fd = 0;
+            if(snprintf(path, sizeof(path), "%s/%s", dir, (*setting)->name) == -1) {
+                fprintf(stderr, "snprintf failed: %m\n");
+                return -1;
+            }
+            if(write(fd, (*setting)->value, strlen((*setting)->value)) == -1) {
+                fprintf(stderr, "writing to %s failed: %m\n", path);
+                close(fd);
+                return -1;
+            }
+            close(fd);
+        }
+    }
+    fprintf(stderr, "done\n");
+    
+    fprintf(stderr, "=> setting rlimit...");
+    if(setrlimit(RLIMIT_NOFILE, &(struct rlimit) {
+                .rlim_max = FD_COUNT,
+                .rlim_cur = FD_COUNT,
+                })) {
+        fprintf(stderr, "failed: %m\n");
+        return 1;
+    }
+    fprintf(stderr, "done\n");
+    return 0;
+}
+
+
+
 
 // child
 #define USERNS_OFFSET 10000
